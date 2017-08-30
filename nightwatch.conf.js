15718f29b00d55db5c0765b0ae4804ce8cfa5924
lwj <1198275198@qq.com>
2017-08-30 12:39:36 +0800

    Feat: set firefox driver

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -9,5 +9,8 @@ module.exports = (function (settings) {
     // 设置 Chrome Driver, 让 Selenium 有打开 Chrome 浏览器的能力。
     settings.selenium.cli_args['webdriver.chrome.driver'] = `${path.resolve()}/node_modules/selenium-standalone/.selenium/chromedriver/${seleniumConfig.driver.chrome.version}-${seleniumConfig.driver.chrome.arch}-chromedriver`
 
+    // 设置 Firefox Driver, 让 Selenium 有打开 Firefox 浏览器的能力。
+    settings.selenium.cli_args['webdriver.gecko.driver'] = `${path.resolve()}/node_modules/selenium-standalone/.selenium/geckodriver/${seleniumConfig.driver.firefox.version}-${seleniumConfig.driver.firefox.arch}-geckodriver`
+
     return settings;
 })(require('./nightwatch.json'))
